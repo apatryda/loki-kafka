98c508f2cc80b2f8aa5556bf7a8795f2a875938d
Artur Nerkowski <artur@reaktor5.com>
2017-08-07 06:47:08 +0200

    query operator in progress

--- query-operator.js
+++ query-operator.js
@@ -10,53 +10,94 @@ const hashQuery = require('./hashQuery');
 class QueryOperator {
 
   queries = {};
-  queryQueue = [];
-  sliceSize = 10;
+  processQueue = []
+  // postOperationQueue =
+  opQueue = [];
+  // queryQueue = [];
+  // sliceSize = 10;
 
-  constructor() {
-    const query = {};
-    const queryDigest = hashQuery(query);
-    const dv = this.collection.addDynamicView('all');
+  constructor({
+    collection,
+    queryTopic,
+    resultTopic,
+  }) {
+    this.collection = collection;
+    this.queryTopic = queryTopic;
+    this.resultTopic = resultTopic;
+    this.consumer = new Kafka.SimpleConsumer();
+    this.producer = new Kafka.Producer();
 
-    dv.on('rebuild', (ev) => {
-      const data = dv.data().slice(0, this.sliceSize);
+    const consumerInit = consumer.init();
+    const producerInit = producer.init();
 
-      if (!data.length && this.queries[]) {
-        this.produceFindStop(queryDigest);
-      }
-
-      const _ids = data.map(doc => doc._id);
-
-      this.produceData(data);
-
-      this.findAndRemove({
-        _id: { $in: _ids },
-      });
-    });
-
-    dv.applyFind(query);
+    Promise
+      .resolve(producerInit)
+      .tap(() => {
+        Promise
+          .resolve(consumerInit)
+          .then(() => consumer.subscribe(
+            this.queryTopic,
+            [0],
+            (...args) => this.processMessages(...args)
+          ))
+        ;
+      })
+      .then(() => this.processLoop())
+      .catch(() => Promise.each([
+        this.consumer.end(),
+        this.producer.end(),
+      ]))
+    ;
   }
 
-  watch() {
-    const dv = this.collection.addDynamicView('all');
+  processMessages(messageSet, topic, partition) {
+    messageSet.forEach((message) => {
+      const opMeta = JSON.parse(m.message.value.toString('utf8'));
+      const { op } = opMeta;
 
-    dv.on('rebuild', (ev) => {
-      const data = dv.data().slice(0, this.sliceSize);
+      if (op === 'insert') {
+        const { _id } = opMeta.document;
+        const query = { _id };
 
-      if (!data.length && this.queries[]) {
-        this.produceFindStop(queryDigest);
+        Object.assign(opMeta, { query });
       }
 
-      const _ids = data.map(doc => doc._id);
+      const { query } = opMeta;
+      const queryDigest = hashQuery(query);
+      Object.assign(opMeta, { queryDigest });
 
-      this.produceData(data);
-
-      this.findAndRemove({
-        _id: { $in: _ids },
-      });
+      this.queueOperation(opMeta);
     });
-    dv.applyFind(query);
   }
+
+  processLoop() {
+    return Promise
+      .try(() => this.dequeueOperation())
+      .then(opMeta => this.postprocessOperation(opMeta))
+      .then(() => this.processLoop())
+    ;
+  }
+
+  // watch() {
+  //   const dv = this.collection.addDynamicView('all');
+
+  //   dv.on('rebuild', (ev) => {
+  //     const data = dv.data().slice(0, this.sliceSize);
+
+  //     if (!data.length && this.queries[]) {
+  //       this.produceFindStop(queryDigest);
+  //     }
+
+  //     const _ids = data.map(doc => doc._id);
+
+  //     this.produceData(data);
+
+  //     this.findAndRemove({
+  //       _id: { $in: _ids },
+  //     });
+  //   });
+  //   dv.applyFind(query);
+  // }
 
   produce(messages) {
     return this.producer.send(messages, {
@@ -70,11 +111,33 @@ class QueryOperator {
     });
   }
 
-  produceData(data) {
-    const messages = _.map(_.castArray(data), (doc) => ({
+  // produceData(data) {
+  //   const messages = _.map(_.castArray(data), (doc) => ({
+  //     value: {
+  //       op: 'upsert',
+  //       value: doc,
+  //     },
+  //   }));
+
+  //   return this.produce(messages);
+  // }
+
+  produceUpserts(documents) {
+    const messages = _.map(_.castArray(documents), (document) => ({
       value: {
         op: 'upsert',
-        value: doc,
+        value: document,
+      },
+    }));
+
+    return this.produce(messages);
+  }
+
+  produceRemoves(_ids) {
+    const messages = _.map(_.castArray(_ids), (_id) => ({
+      value: {
+        op: 'remove',
+        value: _id,
       },
     }));
 
@@ -95,9 +158,9 @@ class QueryOperator {
     }]);
   }
 
-  followQuery(queryDigest) {
+  // followQuery(queryDigest) {
 
-  }
+  // }
 
   excludeCached(query) {
     const docs = this.collection.find(query);
@@ -123,73 +186,112 @@ class QueryOperator {
     ;
   }
 
-  hybridFind(query) {
-    return Promise
-      .try(() => {
-        const data = this.collection.find(query);
-        const _ids = data.map(doc => doc._id);
+  // hybridFind(query) {
+  //   return Promise
+  //     .try(() => {
+  //       const data = this.collection.find(query);
+  //       const _ids = data.map(doc => doc._id);
 
-        return this.queryDataStore({ $and: [
-          query,
-          { _id: { $nin: _ids } },
-        ]})
-      })
-      .each((document) => {
-        const { _id } = document;
-        if (this.collection.count({ _id })) {
-          this.collection.findAndUpdate({ _id }, document);
-        } else {
-          this.collection.insert(document);
-        }
-      })
-      .then(() => this.collection.chain()
-        .find(query)
-        .data()
-      )
+  //       return this.queryDataStore({ $and: [
+  //         query,
+  //         { _id: { $nin: _ids } },
+  //       ]})
+  //     })
+  //     .each((document) => {
+  //       const { _id } = document;
+  //       if (this.collection.count({ _id })) {
+  //         this.collection.findAndUpdate({ _id }, document);
+  //       } else {
+  //         this.collection.insert(document);
+  //       }
+  //     })
+  //     .then(() => this.collection.chain()
+  //       .find(query)
+  //       .data()
+  //     )
+  //   ;
+  // }
+
+  dequeueOperation() {
+    if (!this.opQueue.length) {
+      return new Promise((resolve, reject) => {
+        this.next = resolve;
+        this.end = reject;
+      }).then(() => this.dequeueOperation())
+      ;
+    }
+
+    return Promise
+      .resolve(this.opQueue.shift())
     ;
   }
 
-  queueQuery(queryDigest) {
-    this.queryQueue.push(queryDigest);
+  queueOperation(opMeta) {
+    this.opQueue.push(opMeta);
+    this.next();
   }
 
-  addQuery(query) {
+  // queueQuery(queryMeta) {
+  //   this.queryQueue.push(queryMeta);
+  // }
+
+  // addQuery(query) {
+  //   const queryDigest = hashQuery(query);
+
+  //   if (this.queries[queryDigest]) {
+  //     return;
+  //   }
+
+  //   this.queries[queryDigest] = {
+  //     query,
+  //   };
+
+  //   return Promise
+  //     .try(() => this.produceBegin(queryDigest))
+  //     .then((results) => {
+  //       const [result] = results;
+  //       const { offset } = result;
+  //       this.queries[queryDigest].begin = offset;
+
+  //       const data = this.collection.find(query);
+  //       const _ids = data.map(doc => doc._id);
+
+  //       return this.queryDataStore({ $and: [
+  //         query,
+  //         { _id: { $nin: _ids } },
+  //       ]})
+  //     })
+  //     .then((docs) => {
+  //       queryResults.forEach((doc) => {
+  //         const { _id } = doc;
+
+  //         if (!this.count({ _id })) {
+  //           this.collection.insert(doc);
+  //         }
+  //       });
+
+  //       this.queryQueue.push(queryDigest);
+  //     })
+  //   ;
+  // }
+
+  initFind(query) {
     const queryDigest = hashQuery(query);
 
-    if (this.queries[queryDigest]) {
-      return;
+    let queryMeta = this.queries[queryDigest];
+    if (!queryMeta) {
+      queryMeta = this.queries[queryDigest] = { query };
     }
-
-    this.queries[queryDigest] = {
-      query,
-    };
 
     return Promise
       .try(() => this.produceBegin(queryDigest))
-      .then((results) => {
-        const [result] = results;
-        const { offset } = result;
-        this.queries[queryDigest].begin = offset;
-
-        const data = this.collection.find(query);
-        const _ids = data.map(doc => doc._id);
-
-        return this.queryDataStore({ $and: [
-          query,
-          { _id: { $nin: _ids } },
-        ]})
-      })
-      .then((docs) => {
-        queryResults.forEach((doc) => {
-          const { _id } = doc;
-
-          if (!this.count({ _id })) {
-            this.collection.insert(doc);
-          }
-        });
-
-        this.queryQueue.push(queryDigest);
-      })
+      .then(() => this.queueOperation({
+        promise: Promise
+          .resolve(),
+        query,
+        queryDigest,
+        type: 'find',
+      }))
     ;
   }
 
@@ -205,39 +307,185 @@ class QueryOperator {
 
     return Promise
       .try(() => this.produceBegin(queryDigest))
-      .then(() => this.insertIntoDataStore(query, document))
-      .then(() => this.importQuery(query))
-      .then(() => this.queueQuery(queryDigest))
+      .then(() => this.queueOperation({
+        document,
+        promise: Promise
+          .try(() => this.insertIntoDataStore(query, document)),
+        query,
+        queryDigest,
+        type: 'insert',
+      }))
     ;
   }
 
   initRemove(query) {
+    const queryDigest = hashQuery(query);
 
+    return Promise
+      .try(() => this.produceBegin(queryDigest))
+      .then(() => this.queueOperation({
+        promise: Promise
+          .try(() => this.importQuery(this.excludeCached(query)))
+          .then(() => this.removeFromDataStore(query)),
+        query,
+        queryDigest,
+        type: 'remove',
+      }))
+    ;
   }
 
-  processQuery(queryHash) {
-    const { query } = queryMeta;
+  initUpdate(query, update) {
+    const queryDigest = hashQuery(query);
 
-    const data = this.collection.chain()
-      .find(query)
-      .data()
+    return Promise
+      .try(() => this.produceBegin(queryDigest))
+      .then(() => this.queueOperation({
+        promise: Promise
+          .try(() => this.updateDataStore(query, update)),
+        query,
+        queryDigest,
+        type: 'update',
+        update,
+      }))
+    ;
+  }
+
+  processOperation(opMeta) {
+    const {
+      op,
+      query,
+      queryDigest
+    } = opMeta;
+
+    let opPromise;
+
+    switch (op) {
+      case 'find': {
+        opPromise = () => Promise.resolve();
+        break;
+      }
+
+      case 'insert': {
+        const { document } = opMeta;
+        opPromise = () => this.insertIntoDataStore(query, document);
+        break;
+      }
+
+      case 'remove': {
+        opPromise = () => this.removeFromDataStore(query);
+        break;
+      }
+
+      case 'update': {
+        opPromise = () => this.removeFromDataStore(query);
+        break;
+      }
+    }
+
+     Promise
+      .try(() => this.produceBegin(queryDigest))
+      .then(() => {
+        this.postprocessOperation(opMeta);
+      })
     ;
 
     return Promise
-      .try(() => this.produceData(data))
-      .then(() => this.produceEnd(queryDigest))
+      .try(() => this.produceBegin(queryDigest))
+      .then(() => this.queueOperation({
+        document,
+        promise: Promise
+          .try(() => this.insertIntoDataStore(query, document)),
+        query,
+        queryDigest,
+        type: 'insert',
+      }))
+    ;
+
+    return Promise
+      .try(() => this.produceBegin(queryDigest))
+      .then(() => this.queueOperation({
+        promise: Promise
+          .resolve(),
+        query,
+        queryDigest,
+        type: 'find',
+      }))
+    ;
+
+  }
+
+  postprocessOperation(opMeta) {
+    return Promise
+      // .try(() => opMeta.promise)
+      .try(() => {
+        const { op } = opMeta;
+
+        switch (op) {
+          case 'find':
+          case 'insert':
+          case 'update': {
+            const {
+              type,
+              query,
+              queryDigest,
+            } = queryMeta;
+
+            const queryToImport = type === 'find'
+              ? this.excludeCached(query)
+              : query
+            ;
+
+            Promise
+              .try(() => this.importQuery(queryToImport))
+              .then(() => {
+                const docs = this.collection.chain()
+                  .find(query)
+                  .data({ removeMeta: true })
+                ;
+
+                this.collection.findAndRemove(query);
+                return this.produceUpserts(docs);
+              })
+              .then(() => this.produceEnd(queryDigest))
+            ;
+          }
+
+          case 'remove': {
+            const {
+              query,
+              queryDigest,
+            } = queryMeta;
+
+            const queryToImport = this.excludeCached(query);
+
+            Promise
+              .try(() => {
+                const docs = this.collection.chain()
+                  .find(query)
+                  .data()
+                ;
+                const _ids = docs.map(doc => doc._id);
+
+                this.collection.findAndRemove(query);
+                return this.produceRemoves(_ids);
+              })
+              .then(() => this.produceEnd(queryDigest))
+            ;
+          }
+        }
+      })
     ;
   }
 
-  queryDataStore(query) {
-    throw new Error('Not implemented');
-  }
+  // queryDataStore(query) {
+  //   throw new Error('Not implemented');
+  // }
 
   findInDataStore(query) {
     throw new Error('Not implemented');
   }
 
-  updateDataStore(query, update) {
+  insertIntoDataStore(document) {
     throw new Error('Not implemented');
   }
 
@@ -245,7 +493,7 @@ class QueryOperator {
     throw new Error('Not implemented');
   }
 
-  insertIntoDataStore(document) {
+  updateDataStore(query, update) {
     throw new Error('Not implemented');
   }
 
